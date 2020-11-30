0fc05c9311c16611eeb364273a66e78e7ffc2c99
zulagane <57494449+mustafacanyl@users.noreply.github.com>
2020-12-01 00:03:30 +0200

    Update create.js

--- commands/Giveaways/create.js
+++ commands/Giveaways/create.js
@@ -1,98 +1,107 @@
-const ms = require("ms");
+const Discord = require('discord.js');
+const ms = require('ms');
+const prettyMilliseconds = require('pretty-ms');
 
-exports.run = async (client, message, args, lang) => {
-    let role = client.db.fetch(`role_${message.guild.id}`);
-    if (!role) role = client.config.giveaway.grole;
-    // If the member doesn't have enough permissions
-    if (!message.member.hasPermission("MANAGE_MESSAGES") && !message.member.roles.cache.some((r) => r.name === role))
-        return message.channel.send(lang.create.perms + "** **" + "`" + role + "`" + "!.");
-    
-
-    // Giveaway channel
-    let giveawayChannel = message.mentions.channels.first();
-    // If no channel is mentionned
-    if (!giveawayChannel) return message.channel.send(lang.create.channel)
-        .then(msg => {
-            msg.delete({ timeout: 10000 });
-            message.delete({ timeout: 10000 });
-        });
-
-        if(message.member.guild.id != giveawayChannel.guild.id) return message.channel.send(lang.create.otherServer);
- 
-    // Giveaway duration
-    let giveawayDuration = args[1];
-    // If the duration isn't valid
-    if (!giveawayDuration || isNaN(ms(giveawayDuration))) return message.channel.send(lang.create.duration)
-        .then(msg => {
-            msg.delete({ timeout: 10000 });
-            message.delete({ timeout: 10000 });
-        });
-
-
-    // Number of winners
-    let giveawayNumberWinners = args[2];
-    // If the specified number of winners is not a number
-    if (isNaN(giveawayNumberWinners)) return message.channel.send(lang.create.argswinners)
-        .then(msg => {
-            msg.delete({ timeout: 10000 });
-            message.delete({ timeout: 10000 });
-        });
-
-
-    // Giveaway prize
-    let giveawayPrize = args.slice(3).join(' ');
-    // If no prize is specified
-    if (!giveawayPrize) return message.channel.send(lang.create.prize)
-        .then(msg => {
-            msg.delete({ timeout: 10000 });
-            message.delete({ timeout: 10000 });
-        });
-
-
-    let mention = client.db.fetch(`mention_${message.guild.id}`);
-
-    if (!mention) mention = false;
-    if (mention) {
-        var text1 = "@everyone\n\n" + lang.create.giveaway;
-        var text2 = "@everyone\n\n" + lang.create.giveawayEnded;
+module.exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission('MANAGE_MESSAGES')) {
+      message.channel.send('You don\'t have permission to use this command.');
+      return;
     }
+    message.channel.send('<a:Blob:740349851266580531> Please select the channel you want to host the giveaway\nYou can cancel the giveaway at any time by saying \`cancel\`.');
+    await startMessageCollectors(bot, message, args);
+}
 
-    if (!mention) {
-        var text1 = lang.create.giveaway;
-        var text2 = lang.create.giveawayEnded;
-    }
+module.exports.config = {
+    name: "create",
+    description: "Creates a giveaway step by step.",
+    usage: "p!create",
+    accessableby: "manageServer, manageMessages",
+    aliases: []
+}
 
-    // Start the giveaway
-    client.giveawaysManager.start(giveawayChannel, {
-        // The giveaway duration
-        time: ms(giveawayDuration),
-        // The giveaway prize
-        prize: giveawayPrize,
-        // The giveaway winner count
-        winnerCount: giveawayNumberWinners,
-        // Who hosts this giveaway
-        hostedBy: client.config.giveaway.hostedBy ? message.author : null,
-        // Messages
-        messages: {
-            giveaway: text1,
-            giveawayEnded: text2,
-            timeRemaining: lang.create.timeRemaining,
-            inviteToParticipate: lang.create.inviteToParticipate(message),
-            winMessage: lang.create.winMessage(message),
-            embedFooter: lang.create.embedFooter,
-            noWinner: lang.create.noWinner,
-            hostedBy: lang.create.hostedBy,
-            winners: lang.create.winners,
-            endedAt: lang.create.endedAt,
-            units: {
-                seconds: lang.units.seconds,
-                minutes: lang.units.minutes,
-                hours: lang.units.hours,
-                days: lang.units.days,
-                pluralS: false // Not needed, because units end with a S so it will automatically removed if the unit value is lower than 2
-            }
+function startMessageCollectors(bot, message, args) {
+    let channelFilter = m => m.author.id === message.author.id;
+    let channelCollector = new Discord.MessageCollector(message.channel, channelFilter, { max: 999 });
+
+    channelCollector.on('collect', async msg => {
+      let channel = await msg.mentions.channels.first();
+      if (msg.content.toLowerCase() === 'cancel') {
+        msg.channel.send('The giveaway has been <:Verified:740350406051496067> successfully canceled.')
+        channelCollector.stop();
+        return;
+      }
+      if (!channel) {
+        await msg.channel.send('That is not a valid channel!');
+        await channelCollector.stop();
+        return;
+      } else {
+        msg.channel.send(`<:Blob:740349910905258074> The giveaway will be hosted in ${channel.toString()}. How long do you want the giveaway to last?\nExample: 20m, 1h`)
+        channelCollector.stop();
+      }
+      let durationFilter = m => m.author.id === message.author.id;
+      let durationCollector = new Discord.MessageCollector(message.channel, durationFilter, { max: 999 });
+    durationCollector.on('collect', async msg => {
+        let duration = msg.content;
+        if (msg.content.toLowerCase() === 'cancel') {
+          msg.channel.send('The giveaway has been canceled.')
+          durationCollector.stop();
+          return;
         }
-    }, message.delete({ timeout: 10000 }));
-
-    message.channel.send(`${lang.create.good} ${giveawayChannel}!`);
-};
+        if (!duration || isNaN(ms(duration))) {
+          await msg.channel.send('You did use a valid time format!')
+          durationCollector.stop();
+          return;
+        } else {
+          msg.channel.send(`<a:Blob:740349851266580531> The giveaway will last **${prettyMilliseconds(ms(duration), {verbose: true})}**. How many winners do you want to have?\nThe max amount of winners you can have is 20.`);
+          durationCollector.stop();
+        }
+        let winnersFilter = m => m.author.id === message.author.id;
+        let winnersCollector = new Discord.MessageCollector(message.channel, winnersFilter, { max: 999 });
+    winnersCollector.on('collect', async msg => {
+        let winners = msg.content;
+        let trueWinners = Math.round(winners);
+        if (msg.content.toLowerCase() === 'cancel') {
+          msg.channel.send('The giveaway has been canceled.')
+          winnersCollector.stop();
+          return;
+        }
+        if (isNaN(trueWinners) || (parseInt(trueWinners) <= 0 || trueWinners > 20)) {
+          await msg.channel.send(`You didn't provide a valid amount of winners!`);
+          winnersCollector.stop();
+          return;
+        } else {
+          msg.channel.send(`<a:Blob:740349851266580531> There will be ${trueWinners} winner(s). Now, what do you want the prize to be?`)
+          winnersCollector.stop();
+        }
+        let prizeFilter = m => m.author.id === message.author.id;
+        let prizeCollector = new Discord.MessageCollector(message.channel, prizeFilter, { max: 999 });
+    prizeCollector.on('collect', async msg => {
+        let prize = msg.content;
+        if (msg.content.toLowerCase() === 'cancel') {
+          msg.channel.send('The giveaway has been canceled.')
+          prizeCollector.stop();
+          return;
+        }
+        if (!prize) {
+          await msg.channel.send(`You didn't specify a prize!`)
+          prizeCollector.stop();
+          return;
+        } else {
+          msg.channel.send(`<a:Blob:740349851266580531> The giveaway has been created in ${channel.toString()}.`);
+          prizeCollector.stop();
+          bot.giveawaysManager.start(channel, {
+            time: ms(duration),
+            prize: prize,
+            winnerCount: trueWinners,
+            hostedBy: bot.config.hostedBy ? message.author : null,
+            messages: {
+              messages: { giveaway: text1, giveawayEnded: text2, timeRemaining: lang.create.timeRemaining, inviteToParticipate: lang.create.inviteToParticipate(message), winMessage: lang.create.winMessage(message), embedFooter: lang.create.embedFooter, noWinner: lang.create.noWinner, hostedBy: lang.create.hostedBy, winners: lang.create.winners, endedAt: lang.create.endedAt, units: { seconds: lang.units.seconds, minutes: lang.units.minutes, hours: lang.units.hours, days: lang.units.days, pluralS: false // Not needed, because units end with a S so it will automatically removed if the unit value is lower than 2
+                  }
+              }
+            });
+    }
+  });
+  });
+  });
+  });
+}
