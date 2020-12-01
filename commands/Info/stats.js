20b650b1855ce8179909bd3684bed7a5f012a983
zulagane <57494449+mustafacanyl@users.noreply.github.com>
2020-12-01 11:22:16 +0200

    Update stats.js

--- commands/Info/stats.js
+++ commands/Info/stats.js
@@ -8,20 +8,20 @@ exports.run = async (client, message, args, lang) => {
     let discord_giveaways = require("../../package.json").dependencies["discord-giveaways"];
     let supportURL = client.config.auth.support;
     let web = "http://managegift.ga";
-    let donate = "https://paypal.me/Hadikob";
-    let invite = "https://discordapp.com/oauth2/authorize?client_id=598564396691750933&scope=bot&permissions=8";
+    let donate = "https://top.gg/bot/768801888778715156/vote";
+    let invite = "https://discord.com/oauth2/authorize?client_id=768801888778715156&scope=bot&permissions=8";
     const duration = moment.duration(client.uptime).format(" D [**days**], H [**hours**], m [**minutes**], s [**seconds**]");
     message.channel.send(new MessageEmbed()
         .setTitle(lang.stats.title)
-        .setAuthor(`ManageGift's | Version ${version}`)
+        .setAuthor(`Present Bot's | Version ${version}`)
         .setThumbnail("https://g.top4top.io/s_1695qf6p11.gif")
         .setDescription(lang.stats.info)
         .addField(lang.stats.stats, lang.stats.stat + message.client.guilds.cache.size + `\n` + lang.stats.set + message.client.users.cache.size + `\n` + lang.stats.ch + message.client.channels.cache.size)
-        .addField(lang.stats.ver, `\`Discord.js :\` v${Discord.version}\n\`Nodejs :\` v${process.versions.node}\n\`Discord-giveaways :\` v${discord_giveaways}\n\`ManageGift's :\` v${version}`)
+        .addField(lang.stats.ver, `\`Discord.js :\` v${Discord.version}\n\`Nodejs :\` v${process.versions.node}\n\`Discord-giveaways :\` v${discord_giveaways}\n\`Present Bot's :\` v${version}`)
         .addField(lang.stats.ram, `\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB\``)
         .addField(lang.stats.on, lang.stats.for + `${duration}`)
         .addField(lang.stats.moreinfo, lang.stats.comd + message.client.commands.size + `\n` + lang.stats.giv + client.giveawaysManager.giveaways.length)
-        .addField(lang.help.link, `[Support server](${supportURL}) ● [Donate](${donate}) ● [Website](${web}) ● [Invite](${invite})`)
+        .addField(lang.help.link, `[Support server](${supportURL}) ● [Vote](${donate}) ● [Website](${web}) ● [Invite](${invite})`)
         .setColor(client.config.embeds.color)
         .setFooter(client.config.embeds.footers));
 }
